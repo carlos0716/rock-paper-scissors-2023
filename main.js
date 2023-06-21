98304de061031131eb54017ad399eb99971dca56
carlos0716 <chulezyc@gmail.com>
2023-06-21 16:38:47 -0600

    makes playerSelection case insensitive

--- main.js
+++ main.js
@@ -9,4 +9,21 @@ return computerChoice;
 function randomNumber() {
     return Math.floor(Math.random() * 3); // creates a random number between 0 and 2
   }
-  
+ 
+function oneRound(playerSelection, computerSelection) {
+    
+
+    if ((playerSelection === 'rock') && (computerSelection === 'rock')) {
+        console.log('It is a Draw');
+    } else {
+        console.log('test');
+    }
+}  
+
+function playerSelection() {
+    let playerSelection = prompt('Enter Selection');
+    let playerSelectionInsensitive = playerSelection.toLowerCase();
+    return playerSelectionInsensitive;
+}
+
+//oneRound(playerSelection(), getComputerChoice());
