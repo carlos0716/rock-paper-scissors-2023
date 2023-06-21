bd9b6ce2f52c10afaa9facfcaf67bfbce0b29d79
carlos0716 <chulezyc@gmail.com>
2023-06-21 16:08:00 -0600

    adds function getComputerChoice

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+const choices = ['rock', 'paper', 'scissors'];// array containing the possible choices, it is contained into an array to asign an index number to every choice with which will generate the random number.
+						
+function getComputerChoice() {
+let computerChoice = choices[randomNumber()]; //uses the index to acces the element selected randomly
+return computerChoice;
+}
+
+
 function randomNumber() {
     return Math.floor(Math.random() * 3); // creates a random number between 0 and 2
   }
