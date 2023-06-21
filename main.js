b1df3e848f0ed815dcad584abbbcb0e5a096efb0
carlos0716 <chulezyc@gmail.com>
2023-06-21 17:31:47 -0600

    adds logic if the player or cpu wins or if its a draw

--- main.js
+++ main.js
@@ -1,20 +1,45 @@
 const choices = ['rock', 'paper', 'scissors'];// array containing the possible choices, it is contained into an array to asign an index number to every choice with which will generate the random number.
 						
-function getComputerChoice() {
-let computerChoice = choices[randomNumber()]; //uses the index to acces the element selected randomly
-return computerChoice;
-}
 
-
-function randomNumber() {
-    return Math.floor(Math.random() * 3); // creates a random number between 0 and 2
-  }
  
 function oneRound(playerSelection, computerSelection) {
-    if (playerSelection === '') {
-        alert('Try again');
-    } 
+const playerWinsMessage = `Player - ${playerSelection} vs CPU - ${computerSelection} - Player wins!`;
+const cpuWinsMessage = `CPU - ${computerSelection} vs  Player -${playerSelection} - CPU wins!`;
+    
+if (playerSelection === '') {
+    alert('Try again');
+    } else if (playerSelection === computerSelection)  {
+        console.log(`Player - ${playerSelection} vs CPU ${computerSelection} - It is a Draw`);
+    } else if(playerSelection === 'rock') {
+            if(computerSelection === 'scissors'){
+                console.log(playerWinsMessage);
+            } else {
+                console.log(cpuWinsMessage);
+            }
+    } else if(playerSelection === 'paper') {
+            if(computerSelection === 'rock') {
+                console.log(playerWinsMessage);
+            } else {
+                console.log(cpuWinsMessage);
+            }
+    } else if(playerSelection === 'scissors') {
+        if(computerSelection === 'paper') {
+            console.log(playerWinsMessage);
+        } else {
+            console.log(cpuWinsMessage);
+        }
+    }
 }  
+
+function getComputerChoice() {
+    let computerChoice = choices[randomNumber()]; //uses the index to acces the element selected randomly
+    return computerChoice;
+    }
+    
+    
+    function randomNumber() {
+        return Math.floor(Math.random() * 3); // creates a random number between 0 and 2
+      }
 
 function playerSelection() {
     let playerSelection = prompt('Enter Selection');
@@ -22,7 +47,6 @@ function playerSelection() {
     return playerSelectionInsensitive;
 }
 
-const playerWinsMessage = `Player wins!`;
-const cpuWinsMessage = `CPU wins!`;
+
 
 //oneRound(playerSelection(), getComputerChoice());
