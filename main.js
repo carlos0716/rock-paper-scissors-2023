f81d5bb1d2b7009d9f7e5b94ba7001e93a2ac0bc
carlos0716 <chulezyc@gmail.com>
2023-06-22 12:48:27 -0600

    function game() added

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
-es = ['rock', 'paper', 'scissors'];// array containing the possible choices, it is contained into an array to asign an index number to every choice with which will generate the random number.
-						
+const choices = ['rock', 'paper', 'scissors'];// array containing the possible choices, it is contained into an array to asign an index number to every choice with which will generate the random number.
+let playerScore = 0;
+let computerScore = 0; 
+function game(){
+    let score = 0;
+    
+    oneRound('scissors', "rock");
+    console.log(playerScore, computerScore);
+    if (playerScore === 5) {
+        console.log('Player Wins')
+        playerScore = playerScore - playerScore;
+        computerScore = computerScore - computerScore;
+    } else if(computerScore === 5) {
+        console.log('CPU Wins')
+        playerScore = playerScore - playerScore;
+        computerScore = computerScore - computerScore;
+    }
+
+}
  
 function oneRound(playerSelection, computerSelection) {
-const playerWinsMessage = `Player - ${playerSelection} CPU - ${computerSelection} - Player wins!`;
-const cpuWinsMessage = `CPU - ${computerSelection} Player - ${playerSelection} - CPU wins!`;
+const playerWinsMessage = `${playerSelection} beats ${computerSelection} - Player wins!`;
+const cpuWinsMessage = `${computerSelection} beats ${playerSelection} - CPU wins!`;
 
-let playerScore = 0;
-let computerScore = 0;  
-  
+   
 if (playerSelection === '') {
     alert('Try again');
     } else if (playerSelection === computerSelection)  {
-        console.log(`Player - ${playerSelection} vs CPU ${computerSelection} - It is a Draw`);
+        console.log(`It is a Draw`);
     } else if(playerSelection === 'rock') {
             if(computerSelection === 'scissors'){
                 console.log(playerWinsMessage);
@@ -36,10 +51,20 @@ if (playerSelection === '') {
             console.log(cpuWinsMessage);
             computerScore++;
         }
+    } else if(player !== 'rock'){
+        alert('Wrong choice, try again')
+    } else if(player !== 'paper'){
+        alert('Wrong choice, try again')
+    } else if(player !== 'scissors'){
+        alert('Wrong choice, try again')
     }
-    let score = `Player ${playerScore} - CPU ${computerScore}`
-    console.log(score);
+    //console.log(computerScore, playerScore);
 }  
+
+
+
+
+//oneRound(playerSelection(), getComputerChoice());
 
 function getComputerChoice() {
     let computerChoice = choices[randomNumber()]; //uses the index to acces the element selected randomly
