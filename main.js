0f060938e2aeb12f4ad2a5b91f5f6e2ea5ac06de
carlos0716 <chulezyc@gmail.com>
2023-07-28 11:56:10 -0600

    eventListener added to each button

--- main.js
+++ main.js
@@ -3,60 +3,28 @@ const choices = ['rock', 'paper', 'scissors'];
 let playerScore = 0; // the variables to keep the score are set into the global object so they can be accessed by the two functions.
 let computerScore = 0; 
 
-function game(){
-    oneRound(playerSelection(), getComputerChoice());
-    console.log(playerScore, computerScore);
-    if (playerScore === 5) {
-        console.log('Player Wins - Game Over')
-        playerScore = playerScore - playerScore;
-        computerScore = computerScore - computerScore;
-    } else if(computerScore === 5) {
-        console.log('CPU Wins - Game Over')
-        playerScore = playerScore - playerScore;
-        computerScore = computerScore - computerScore;
-    }
-}
- 
-function oneRound(playerSelection, computerSelection) {
-const playerWinsMessage = `${playerSelection} beats ${computerSelection} - Player wins!`;
-const cpuWinsMessage = `${computerSelection} beats ${playerSelection} - CPU wins!`;
 
-   if (playerSelection === '') {
-    alert('Try again');
-    } else if (playerSelection === computerSelection)  {
-        console.log(`It is a Draw`);
-    } else if(playerSelection === 'rock') {
-            if(computerSelection === 'scissors'){
-                console.log(playerWinsMessage);
-                playerScore++;
-            } else {
-                console.log(cpuWinsMessage);
-                computerScore++;
-            }
-    } else if(playerSelection === 'paper') {
-            if(computerSelection === 'rock') {
-                console.log(playerWinsMessage);
-                playerScore++;
-            } else {
-                console.log(cpuWinsMessage);
-                computerScore++;
-            }
-    } else if(playerSelection === 'scissors') {
-        if(computerSelection === 'paper') {
-            console.log(playerWinsMessage);
-            playerScore++;
-        } else {
-            console.log(cpuWinsMessage);
-            computerScore++;
-        }
-    } else if(playerSelection !== 'rock'){
-        alert('Wrong choice, try again')
-    } else if(playerSelection !== 'paper'){
-        alert('Wrong choice, try again')
-    } else if(playerSelection !== 'scissors'){
-        alert('Wrong choice, try again')
-    }
-}  
+
+
+
+
+//rockBtn.addEventListener('click', oneRound);
+// function game(){
+//     oneRound(playerSelection(), getComputerChoice());
+//     console.log(playerScore, computerScore);
+//     if (playerScore === 5) {
+//         console.log('Player Wins - Game Over')
+//         playerScore = playerScore - playerScore;
+//         computerScore = computerScore - computerScore;
+//     } else if(computerScore === 5) {
+//         console.log('CPU Wins - Game Over')
+//         playerScore = playerScore - playerScore;
+//         computerScore = computerScore - computerScore;
+//     }
+// }
+ 
+
+
 
 function getComputerChoice() {
     let computerChoice = choices[randomNumber()]; 
@@ -64,11 +32,61 @@ function getComputerChoice() {
 }
     
 function randomNumber() {
-        return Math.floor(Math.random() * getComputerChoice.length); 
+        return Math.floor(Math.random() * choices.length); 
 }
 
-function playerSelection() {
-    let playerSelection = prompt('Enter Selection');
-    let playerSelectionInsensitive = playerSelection.toLowerCase();
-    return playerSelectionInsensitive;
-}
+const rock = document.getElementById('rock');
+rock.addEventListener('click', (e) =>{
+    oneRound('rock', getComputerChoice());
+} );
+
+const paper = document.getElementById('paper');
+paper.addEventListener('click', (e) =>{
+    oneRound('paper', getComputerChoice());
+} );
+
+const scissors = document.getElementById('scissors');
+scissors.addEventListener('click', (e) =>{
+    oneRound('scissors', getComputerChoice());
+} );
+
+
+function oneRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+         console.log('Draw');
+    }
+     else if (playerSelection === 'rock') {
+     if (computerSelection === 'paper') {
+         console.log('cpu wins');
+         computerScore++;
+     } else if (computerSelection === 'scissors') {
+         console.log('cpu lose');
+         playerScore++;
+     }
+    } else if(playerSelection === 'paper') {
+        if (computerSelection === 'scissors') {
+            console.log('cpu wins');
+            computerScore++;
+        } else if (computerSelection === 'rock') {
+            console.log('cpu lose');
+            playerScore++;
+        }
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            console.log('cpu wins');
+            computerScore++;
+        } else if (computerSelection === 'paper') {
+            console.log('cpu lose');
+            playerScore++;
+        }
+    }
+    return playerScore;
+ } ;
+// function playerSelection() {
+//     let playerSelection = 
+// }
+
+// function playRock(e) {
+//     let value = 'rock';
+//     console.log(value);
+// }
